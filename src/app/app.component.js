1ac8c24d5b275755fc0f66e2f330e9f445d6ab3a
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-09-28 13:02:00 -0400

    played with funtcins and biuttons

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,40 +1,63 @@
 // Assuming you have an AngularJS app defined
-var app = angular.module('myApp', []);
-
+app.controller('MyController', function ($scope) {
 // Define a controller
-app.controller('myController', function($scope) {
-  $scope.toggleDescription = function() {
-    const description = angular.element(document.querySelector('#description'));
-    // Toggle the visibility of the description
-    if (description.text() === 'none' || description.text() === '') {
-      description.text(original); // Show the description
-    } else {
-      description.text(''); // Hide the description
-    }
-  };
-
-  $scope.duplicateCard = function() {
-    const clone = angular.element(document.querySelector('.card')).clone(); // Clone the whole card
-    angular.element(document.querySelector('body')).append(clone);
-  };
-
-  $scope.changeBackgroundColor = function() {
-    const card = angular.element(document.querySelector('.card'));
-    // Change background color
-    card.css('background-color', '#' + randomColor());
-  };
-
-  $scope.changeHeading = function() {
-    const title = angular.element(document.querySelector('h2'));
-    title.text('Super Pod 2 Realty');
-  };
-
-  $scope.deleteCard = function() {
-    const cards = angular.element(document.querySelectorAll('.card'));
-    // Check if there are cards to delete
-    if (cards.length > 1) {
-      const lastCard = cards[cards.length - 1];
-      lastCard.remove();
-    }
-  };
+//duplicate button
+$scope.duplicatecard = function () {
+document.querySelector('#btn').addEventListener('click', () => {
+  const clone = document.querySelector('.card').cloneNode(true); // Clone the whole card
+  document.body.appendChild(clone);
 });
+};
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var letters = ["A", "B", "C", "D", "E", "F"];
+
+function randomColor() {
+  var color = "";
+  for (var i = 0; i < 3; i++) {
+    var index = Math.floor(Math.random() * (numbers.length + letters.length));
+    color += (index < numbers.length) ? numbers[index] : letters[index - numbers.length];
+  }
+  return color;
+}
+$scope.changeBGcolor = function () {
+document.querySelector('#colorBtn').addEventListener('click', () => {
+  const card = document.querySelector('.card');
+  // Change background color
+  card.style.backgroundColor = '#' + randomColor();
+});
+};
+
+$scope.changeheading = function () {
+// HEADING CHANGER
+document.querySelector('#Headingbtn').addEventListener('click', () => {
+  const title = document.querySelector('h2');
+  title.innerText = "Super Pod 2 Realty";
+});};
+
+$scope.deletecard = function () {
+// Card delete
+document.querySelector('#dltbtn').addEventListener('click', () => {
+  const cards = document.querySelectorAll('.card');
+  // Check if there are cards to delete
+  if (cards.length > 1) {
+    const lastCard = cards[cards.length - 1];
+    lastCard.parentNode.removeChild(lastCard);
+  }
+});};
+
+$scope.toggleDescription = function () {
+var original = document.querySelector('#description').innerText;
+
+document.querySelector('#detailsBtn').addEventListener('click', () => {
+  const description = document.querySelector('#description');
+  // Toggle the visibility of the description
+  if (description.innerText === 'none' || description.innerText === '') {
+    description.innerText = original; // Show the description
+  } else {
+    description.innerText = ''; // Hide the description
+  }
+});
+};
+
+})
